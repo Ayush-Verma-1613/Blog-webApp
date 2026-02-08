95d3f8775f6e43741ce4b99ab9d9d115104cd208
Ayush-Verma-1613 <ayushverma1613@gmail.com>
2026-02-08 16:35:56 +0530

    [fix the issue]

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,16 +1,20 @@
-
 import Blog from '@/components/blog';
+
+export const dynamic = "force-dynamic";
 
 export const metadata = {
   title: "Blogs",
   description: "Read latest blogs",
 };
 
-const BlogRouter = async() => {
-  const blog = await fetch(`${process.env.SERVER}/api/blog`)
-  const data = await blog.json()
-  return(<Blog data={data}/>
-  )
-}
+const BlogRouter = async () => {
+  const blog = await fetch(`${process.env.SERVER}/api/blog`, {
+    cache: "no-store",
+  });
+
+  const data = await blog.json();
+
+  return <Blog data={data} />;
+};
 
 export default BlogRouter;
